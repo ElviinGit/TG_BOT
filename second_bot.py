057d58b4d6d180e48b98340d065606bdfbfdd383
Elvin <fromazerbaijantogermany@gmail.com>
2026-05-12 09:34:19 +0400

    try except for debugging added

--- second_bot.py
+++ second_bot.py
@@ -1,7 +1,10 @@
 import undetected_chromedriver as uc
 import time
 driver = uc.Chrome()
-driver.get("https//turbo.az")
-time.sleep(10)
+try:
+    driver.get("https//turbo.az")
+    time.sleep(10)
+except Exception as e:
+        driver.save_screenshot("debug_error.png")
 driver.quit()
 print(" closed after 10 second")

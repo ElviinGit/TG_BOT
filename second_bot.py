d3dc2332d2ec9b0c45e3eca9018297deaa6eba8d
Elvin <fromazerbaijantogermany@gmail.com>
2026-05-12 11:00:00 +0400

    optiona added for uc

--- second_bot.py
+++ second_bot.py
@@ -1,5 +1,12 @@
 import undetected_chromedriver as uc
 import time
+options = uc.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--window-size=1920,1080")
+
 with uc.Chrome(version_main=147) as driver:
     driver.get("https://turbo.az")
     time.sleep(10)
